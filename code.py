3a032500a3ec3dfa9eb86532e043a048c13edb1c
AYANSK11 <92205708+ayansk11@users.noreply.github.com>
2022-02-22 19:48:23 +0530

    Update code.py

--- code.py
+++ code.py
@@ -95,6 +95,44 @@ def april():
     plt.legend()
     plt.show()
     main()
+    
+    
+def may():
+
+       df = pd.read_csv('covid.csv')
+
+       a = list(df.iloc[90:104,0])
+       b = list(df.iloc[90:104,1])
+       c = list(df.iloc[90:104,2])
+       d = list(df.iloc[90:104,3])
+       e = list(df.iloc[90:104,4])
+       f = list(df.iloc[90:104,5])
+       g = list(df.iloc[90:104,6])
+       
+       x_index = np.arange(0, 34, 1)
+
+       plt.style.use("fivethirtyeight")
+
+       plt.figure(figsize=(15, 10), dpi=80)
+
+       plt.xticks(ticks=x_index)
+       plt.plot(a, b, marker="o", label="Daily Confirmed")
+       plt.plot(a, c, marker="o", label="Total Confirmed")
+       plt.plot(a, d, marker="o", label="Daily Recovered")
+       plt.plot(a, e, marker="o", label="Total Recovered")
+       plt.plot(a, f, marker="o", label="Daily Deceased")
+       plt.plot(a, g, marker="o", label="Total Deceased")
+       
+
+       plt.title("----COVID-19----")
+       plt.xlabel("May")
+       plt.ylabel("Number of Cases")
+
+       plt.grid(True, color='k', linestyle=':')
+       plt.tight_layout()
+       plt.legend()
+       plt.show()
+       main()
 
 
 def main():
